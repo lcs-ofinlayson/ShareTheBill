6f437916ff8efab9039ecdd5358e9cfa9023ba14
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-02-02 15:03:05 -0500

    Moved the source of truth, out of calculation view and into the app entry point file.

--- ShareTheBill/ShareTheBillApp.swift
+++ ShareTheBill/ShareTheBillApp.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 @main
 struct ShareTheBillApp: App {
+    
+    //MARK: Stored properties
+    
+    //Source fo truth: Where the data is created
+    @State var history: [Result] = []   // Begins as empty list
+
+    
+    
+    //MARK: Computed properties
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                CalculationView()
+                CalculationView(history: $history)
             }
         }
     }
